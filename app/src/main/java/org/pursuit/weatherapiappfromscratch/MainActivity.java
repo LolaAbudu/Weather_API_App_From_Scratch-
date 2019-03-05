aae1c4e88db8f4a38d4cb305c4efaf81502982b5
LolaAbudu <lolaabudu1589@gmail.com>
2019-03-04 21:52:09 -0500

    updated Main Attivity, View Holder and xml

--- app/src/main/java/org/pursuit/weatherapiappfromscratch/MainActivity.java
+++ app/src/main/java/org/pursuit/weatherapiappfromscratch/MainActivity.java
@@ -2,12 +2,16 @@ package org.pursuit.weatherapiappfromscratch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.util.Log;
+
+import android.support.v7.widget.RecyclerView;
 
 import org.pursuit.weatherapiappfromscratch.model.Periods;
 import org.pursuit.weatherapiappfromscratch.model.Weather;
 import org.pursuit.weatherapiappfromscratch.networks.RetrofitSingleton;
 import org.pursuit.weatherapiappfromscratch.networks.WeatherService;
+import org.pursuit.weatherapiappfromscratch.rv.WeatherAdapter;
 
 import java.util.List;
 
@@ -18,10 +22,13 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
         Retrofit retrofit = RetrofitSingleton.getOneInstance();
         WeatherService weatherService = retrofit.create(WeatherService.class);
@@ -31,6 +38,11 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Weather> call, Response<Weather> response) {
                 List<Periods> periodsList = response.body().getResponse().get(0).getPeriods();
                 Log.d("TAG", "onResponse" + response.body().getResponse().get(0));
+
+                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+                WeatherAdapter weatherAdapter = new WeatherAdapter(periodsList);
+                recyclerView.setLayoutManager(linearLayoutManager);
+                recyclerView.setAdapter(weatherAdapter);
             }
 
             @Override
--- app/src/main/java/org/pursuit/weatherapiappfromscratch/rv/WeatherViewHolder.java
+++ app/src/main/java/org/pursuit/weatherapiappfromscratch/rv/WeatherViewHolder.java
@@ -6,8 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.squareup.picasso.Picasso;
 
 import org.pursuit.weatherapiappfromscratch.R;
 import org.pursuit.weatherapiappfromscratch.model.Periods;
@@ -18,6 +22,7 @@ public class WeatherViewHolder extends RecyclerView.ViewHolder {
     private ImageView iconImageView;
     private TextView maxTempTextView;
     private TextView minTempTextView;
+    private ToggleButton toggleButton;
 
     public WeatherViewHolder(@NonNull View itemView) {
         super(itemView);
@@ -26,10 +31,38 @@ public class WeatherViewHolder extends RecyclerView.ViewHolder {
         iconImageView = itemView.findViewById(R.id.icon_image_view);
         maxTempTextView = itemView.findViewById(R.id.max_temp_text_view);
         minTempTextView = itemView.findViewById(R.id.min_temp_text_view);
-
+        toggleButton = itemView.findViewById(R.id.toggleButton);
     }
 
     public void onBind(final Periods periods) {
+        dateTextView.setText(periods.getDateTimeISO());
 
+        String maxTempString = "High: " + String.valueOf(periods.getMaxTempF() + " F");
+        String minTempString = "Low: " + String.valueOf(periods.getMinTempF() + "F");
+        maxTempTextView.setText(maxTempString);
+        minTempTextView.setText(minTempString);
+
+        if(periods.getIcon().equals("pcloudy.png")){
+            Picasso.get().load(R.drawable.pcloudy).into(iconImageView);
+        }else if(periods.getIcon().equals("wintrymix.png")){
+            Picasso.get().load(R.drawable.wintrymix).into(iconImageView);
+        }
+
+//        Picasso.get().load(periods.getIcon()).into(iconImageView);
+
+//        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if(isChecked){
+//                    toggleButton.setTextOff("Show Farenheit");
+//                    maxTempTextView.setText(periods.getMaxTempC());
+//                    minTempTextView.setText(periods.getMinTempC());
+//                }else{
+//                    toggleButton.setTextOn("Show Celcius");
+//                    maxTempTextView.setText(periods.getMaxTempF());
+//                    minTempTextView.setText(periods.getMinTempF());
+//                }
+//            }
+//        });
     }
 }
